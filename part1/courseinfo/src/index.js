bc9848fe7bd8357cb7c3666d915e970a9560791d
MirSzabo <poradova.m@gmail.com>
2020-01-27 14:01:43 +0100

    1.2 part

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -10,19 +10,22 @@ const Header = props => {
   );
 };
 
+const Part = props => {
+  console.log(props);
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
 const Content = props => {
   console.log(props);
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+     <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   );
 };
