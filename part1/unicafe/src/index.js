4c49e05f3f665ce705abb285cbf4becc4de42cdc
MirSzabo <poradova.m@gmail.com>
2020-02-01 23:13:13 +0100

    count buttons

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,19 +1,28 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
   // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      code here
+      <div>
+        <h2>give feedback</h2>
+        <button onClick = {() => setGood(prevCount => prevCount +1)}>good</button>
+        <button onClick = {() => setNeutral(prevCount => prevCount +1)}>neutral</button>
+        <button onClick = {() => setBad(prevCount => prevCount +1)}>bad</button>
+      </div>
+      <div>
+        <h2>statistics</h2>
+        <p>good: {good}</p>
+        <p>neutral: {neutral}</p>
+        <p>bad: {bad}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById("root"));
