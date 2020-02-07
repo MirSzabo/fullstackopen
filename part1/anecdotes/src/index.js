804cee5f10d398fa725972bbdaf41e5c9855dc5b
MirSzabo <poradova.m@gmail.com>
2020-02-08 00:18:44 +0100

    anecdote update

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,34 +1,44 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-const App = (props) => {
-  const [selected, setSelected] = useState(0)
+const App = props => {
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(0);
 
   const changeAnecdote = () => {
-    setSelected([
-      ...todos,
-      { id: `${getNewId()}`, description: "random text", completed: false }
-    ]);
+    setSelected(Math.floor(Math.random() * 5));
   };
+
+  const voteForAnecdote = () => {
+    console.log("vote");
+  };
+
+  /*const points = [1, 4, 6, 3]
+
+const copy = [...points]
+// increment the value in position 2 by one
+copy[2] += 1 */
 
   return (
     <div>
-      {props.anecdotes[selected]}
-      <button onClick={() => changeAnecdote()}>Next anecdote</button>
+      <h1>Anecdote of the day</h1>
+      <p>{props.anecdotes[selected]}</p>
+      <p>has {votes} votes</p>
+      <button onClick={voteForAnecdote}>Vote</button>
+      <button onClick={changeAnecdote}>Next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+
     </div>
-  )
-}
+  );
+};
 
 const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."
+];
 
-ReactDOM.render(
-  <App anecdotes={anecdotes} />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById("root"));
