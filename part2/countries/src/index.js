dfb2f043cf392556239c12d1a4962803d44e8b77
MirSzabo <poradova.m@gmail.com>
2020-05-26 23:57:26 +0200

    countries started

---
+++ part2/countries/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const Countries = () => {
+  return (
+    <div>
+      <App />
+    </div>
+  );
+};
+
+ReactDOM.render(<Countries />, document.getElementById("root"));
