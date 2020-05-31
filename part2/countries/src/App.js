302ea8cf640756c2f50a3389b7ff96b4d4770a90
MirSzabo <poradova.m@gmail.com>
2020-05-31 23:56:59 +0200

    fetch countries

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [countries, setCountries] = useState([]);
+
+ // const filtered = countries.filter((country) => country.name.includes(query));
+
+  useEffect(() => {
+    axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+      setCountries((country) => country.concat(response.data));
+    });
+  }, []);
+
   return (
     <div>
-      <h2>find countries</h2>
+      <h2>find countries</h2>    
     </div>
   );
 };
