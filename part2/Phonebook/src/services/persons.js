feb02916529eedf0852b0ae8f2f6ab2d3ef9ee73
MirSzabo <poradova.m@gmail.com>
2020-08-31 00:46:35 +0200

    catch error handling added

--- part2/Phonebook/src/services/persons.js
+++ part2/Phonebook/src/services/persons.js
@@ -2,28 +2,37 @@ import axios from "axios";
 const baseUrl = "http://localhost:3001/persons";
 
 const getAll = () => {
-  const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
+  try {
+    const request = axios.get(baseUrl);
+    return request.then((response) => response.data);
+  } catch (err) {
+    console.log(err);
+    return;
+  }
 };
 
 const create = (person) => {
-  const request = axios.post(baseUrl, person);
-  return request.then((response) => response.data);
+  try {
+    const request = axios.post(baseUrl, person);
+    return request.then((response) => response.data);
+  } catch (err) {
+    console.log(err);
+    return;
+  }
 };
 
 const remove = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`);
-  return request.then((response) => response.data);
-};
-
-const update = (id, person) => {
-  const request = axios.put(`${baseUrl}/${id}`, person);
-  return request.then((response) => response.data);
+  try {
+    const request = axios.delete(`${baseUrl}/${id}`);
+    return request.then((response) => response.data);
+  } catch (err) {
+    console.log(err);
+    return;
+  }
 };
 
 export default {
   getAll,
   create,
   remove,
-  update,
 };
